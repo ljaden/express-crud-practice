7750773f997176f1bbac9b48a048cf6c2ee7fbca
jaden <ljaden.9@gmail.com>
2022-07-11 11:25:41 -0400

    removed test schema

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const app = require("./app");
 
 const db = require("./config/db");
-const tourSchema = new db.Schema({
-  name: String,
-});
-const Tour = db.model("tour", tourSchema);
+// const tourSchema = new db.Schema({
+//   name: String,
+// });
+// const Tour = db.model("tour", tourSchema);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
