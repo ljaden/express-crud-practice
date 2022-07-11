83f4da374716f67729411794a87ab74fe6521e7d
jaden <ljaden.9@gmail.com>
2022-07-11 10:50:06 -0400

    added more data schemas

--- schema/toursSchema.js
+++ schema/toursSchema.js
@@ -5,15 +5,50 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "<name is required>"],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, "<Duration is required>"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "<Group size is required>"],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
   },
+  ratingsQuantity: {
+    type: Number,
+    defualt: 0,
+  },
   price: {
     type: Number,
-    required: [true, "<price is required>"],
+    required: [true, "<Price is required>"],
   },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "<Summary is required>"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "<ImageCover is required>"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // model
